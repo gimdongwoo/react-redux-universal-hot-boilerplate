77963cf658b6df8d65cd8bc7632433cf9f6af225
InGrowth <gimdongwoo@gmail.com>
2017-05-04 12:12:58 +0900

    packages update

--- src/i18n-client.js
+++ src/i18n-client.js
@@ -1,17 +1,32 @@
 import i18n from 'i18next';
 
-i18n
-  .init({
-    whitelist: ['en', 'ko', 'ko-KR'],
-    fallbackLng: 'en',
+const param = {
+  whitelist: ['en', 'ko'],
+  fallbackLng: 'en',
 
-    // have a common namespace used around the full app
-    ns: ['common'],
-    defaultNS: 'common',
+  // have a common namespace used around the full app
+  ns: ['common'],
+  defaultNS: 'common',
 
-    interpolation: {
-      escapeValue: false // not needed for react!!
+  interpolation: {
+    escapeValue: false // not needed for react!!
+  }
+};
+
+// hmr
+if (__DEVELOPMENT__) {
+  param.resources = {
+    en: {
+      common: require('../locales/en/common.json')
+    },
+    ko: {
+      common: require('../locales/ko/common.json')
     }
-  });
+  };
+}
+
+// init
+i18n
+  .init(param);
 
 export default i18n;
--- src/i18n-server.js
+++ src/i18n-server.js
@@ -2,29 +2,30 @@ import i18n from 'i18next';
 import Backend from 'i18next-node-fs-backend';
 import { LanguageDetector } from 'i18next-express-middleware';
 
+const param = {
+  whitelist: ['en', 'ko'],
+  fallbackLng: 'en',
 
+  // have a common namespace used around the full app
+  ns: ['common'],
+  defaultNS: 'common',
+
+  debug: false,
+
+  interpolation: {
+    escapeValue: false // not needed for react!!
+  },
+
+  backend: {
+    loadPath: 'locales/{{lng}}/{{ns}}.json',
+    jsonIndent: 2
+  }
+};
+
+// init
 i18n
   .use(Backend)
   .use(LanguageDetector)
-  .init({
-    whitelist: ['en', 'ko', 'ko-KR'],
-    fallbackLng: 'en',
-
-    // have a common namespace used around the full app
-    ns: ['common'],
-    defaultNS: 'common',
-
-    debug: true,
-
-    interpolation: {
-      escapeValue: false // not needed for react!!
-    },
-
-    backend: {
-      loadPath: 'locales/{{lng}}/{{ns}}.json',
-      jsonIndent: 2
-    }
-  });
-
+  .init(param);
 
 export default i18n;
